fa9eed0507e51888bc534190cf86745687416925
Lucas-aquiles <lucasechegaray2011@gmail.com>
2026-01-06 17:04:00 -0300

    changes obra lista

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -3,7 +3,7 @@ import { MetadataRoute } from "next";
 export default function sitemap(): MetadataRoute.Sitemap {
   return [
     {
-      url: "https://ObraListapro.com",
+      url: "https://obralista.com.ar",
       lastModified: new Date(),
       changeFrequency: "weekly",
       priority: 1,
